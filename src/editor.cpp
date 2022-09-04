94ef9674b3ff9efe48c75c63892b61ed21298cdf
vakhdmyt06 <vakhdmyt06@gmail.com>
2022-09-04 11:41:08 +0200

    misc stuff

--- src/editor.cpp
+++ src/editor.cpp
@@ -1,5 +1,5 @@
 #include "header/game.h"
-//#ifdef _editor
+#ifdef _editor
 void game::editor(){
     debug_cam();
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Add)) gridsize++; if(sf::Keyboard::isKeyPressed(sf::Keyboard::Subtract)) gridsize--;
@@ -10,7 +10,7 @@ void game::editor(){
     for(auto &editilepos : editbox){
         make_tile(editilepos.x, editilepos.y, sf::Color::White, 1);
     }
-    if(sf::Mouse::isButtonPressed(sf::Mouse::Right)) std::remove(editbox.begin(), editbox.end(), sf::Vector2i(fieldx,fieldy));
+    if(sf::Mouse::isButtonPressed(sf::Mouse::Right)) { std::remove(editbox.begin(), editbox.end(), sf::Vector2i(fieldx,fieldy)); cout<<"Cleared x: "<<fieldx<<", y: "<<fieldy<<endl; }
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::RControl)) editor_print();
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) if(editor_playing) {editor_playing=false; plbox.setPosition(sf::Vector2f(win.getPosition().x/2,win.getPosition().y/2)); } else editor_playing=true;
     if(editor_playing) debug_player();
@@ -24,10 +24,12 @@ bool game::editor_poscheck(int xb, int yb){
 }
 
 void game::editor_print(){
+    cout<<"\n\nspawm_player("<<plbox.getPosition().x<<","<<plbox.getPosition().y<<");"<<endl;
+    cout<<"gridsize="<<gridsize<<";"<<endl;
     for(auto &editilepos : editbox){
-        cout<<"make_tile("<<editilepos.x<<", "<<editilepos.y<<", sf::Color::White, 1)"<<endl;
+        cout<<"make_tile("<<editilepos.x<<", "<<editilepos.y<<", sf::Color::White, 1);"<<endl;
     }
-    cout<<editbox.size()<<" objects"<<endl;
+    cout<<"\n\n"<<editbox.size()<<" objects"<<endl;
     slep(5, 1);
 }
-//#endif
+#endif
